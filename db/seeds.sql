3c30ad8513e246f2f4d75781bcbd0d7540a5b4d3
Sofuto22 <zenigata19@gmail.com>
2023-02-13 15:24:02 -0500

    added seeded data for faster loading

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INFO employees(id, name)
+VALUES
+("Marketing"), ("Sales"), ("Leadership"), ("Financial");
+
+INSERT
+    INFO department(
+        first_name,
+        last_name,
+        salary,
+        employee_id
+    )
+VALUES ("Matthew", "Renald", 40000, 2) ("Lissette", "Arnold", 150000, 3) ("Dave", "Mcdonald", 65000, 1) ("Brendan", "Whizel", 40000, 2) ("Li", "Meng", 55000, 4)
+INSERT
+    INFO role(title, department_id)
+VALUES ("Sales", 2) ("Leadership", 3) ("Marketing", 1) ("Sales", 2) ("Financial", 4)
